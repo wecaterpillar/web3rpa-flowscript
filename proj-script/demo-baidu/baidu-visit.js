e409595426c488527e5e7b99697823a86dc4ffa4
dev <dev@dev.com>
2022-11-27 10:38:27 +0800

    init

---
+++ proj-script/demo-baidu/baidu-visit.js
@@ -0,0 +1,26 @@
+const { getBrowserConfig, launchBrowserContext, closeBrowserContext} = require('../../lib/rpa/browser')
+const { getListData, getDetailData, updateDetailData} = require('../../lib/rpa/dataUtil')
+
+// 浏览器帮助类
+// 数据帮助类，可考虑调用localAPI来同rpaServer交互
+
+const flow_start = ({item}) => {
+    (async () => {
+        console.debug("invoke flow_start")
+        console.debug(item)
+        let browserConfig = await getBrowserConfig(item['browser'])
+        let context = await launchBrowserContext(browserConfig)
+        //console.debug("context")
+        //console.debug(context)
+        const page = await context.newPage();
+        let indexUrl = 'https://www.baidu.com/'
+        //console.debug(indexUrl)
+        await page.goto(indexUrl)
+        //await page.screenshot({path:path.join(rpaConfig.appDataPath, 'logs/1.png')})
+        await closeBrowserContext(context)
+    })()
+}
+
+exports = module.exports = {
+    flow_start: flow_start
+}
