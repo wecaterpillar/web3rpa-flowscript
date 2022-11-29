d3e32c549283e19aa2acdb2b47ae03dc57e9a30a
dev <dev@dev.com>
2022-11-29 20:19:05 +0800

    update rpaDev

--- dev/rpa/rpaDev.js
+++ dev/rpa/rpaDev.js
@@ -1,30 +1,12 @@
 const fs = require("fs")
 const path = require('path')
 
+const { remoteServerInit } = require('./remoteServer')
+const { browserInit } = require('./browser')
+const { dataUtilInit, getListData, getDetailData, updateDetailData, createDetailData, getRpaPlanTaskList} = require('./dataUtil')
+
 
 const rpaConfig = {}
-
-const startRpa = async () => {
-    console.debug('start rpa ...')
-    console.debug(rpaConfig)
-
-    // 1. load rpa config
-    let devConfig = await loadRpaConfig()
-    if(devConfig){
-        Object.assign(rpaConfig, devConfig)
-    }
-
-    // 2. init
-    // 2.1 dataUtil
-    await dataUtilInit(rpaConfig)
-
-    // 2.2 browser
-    await browserInit(rpaConfig)
-
-    // 3. rpa dev?
-}
-
-await startRpa()
 
 const loadRpaConfig = async () => {
     let  rpaConfig
@@ -38,3 +20,25 @@ const loadRpaConfig = async () => {
     })()
     return rpaConfig
 }
+
+const startRpa = async () => {
+    console.debug('start rpa ...')
+    console.debug(rpaConfig)
+
+    // 1. load rpa config
+    let devConfig = await loadRpaConfig()
+    if(devConfig){
+        Object.assign(rpaConfig, devConfig)
+    }
+
+    // 2. init
+    // 2.1 dataUtil
+    dataUtilInit(rpaConfig)
+
+    // 2.2 browser
+    browserInit(rpaConfig)
+
+    // 3. rpa dev?
+}
+
+startRpa()
