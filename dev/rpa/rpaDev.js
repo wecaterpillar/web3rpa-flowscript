6bed17f311648b3bd0e5362ccc4532300bc622ee
dev <dev@dev.com>
2022-11-29 20:55:43 +0800

    update rpaDev

--- dev/rpa/rpaDev.js
+++ dev/rpa/rpaDev.js
@@ -9,27 +9,35 @@ const { dataUtilInit, getListData, getDetailData, updateDetailData, createDetail
 const rpaConfig = {}
 
 const loadRpaConfig = async () => {
-    let  rpaConfig
+    let  devConfig
     // load rpaConfig from dev.json
-    await (async () => {
-        let configPath = './dev.json'
-        if (fs.existsSync(configPath)) {
-            let configStr = fs.readFileSync(configPath)
-            rpaConfig = JSON.parse(configStr)
-        }
-    })()
-    return rpaConfig
+    let configPath = './dev/rpa/dev.json'
+    if (fs.existsSync(configPath)) {
+        let configStr = fs.readFileSync(configPath)
+        devConfig = JSON.parse(configStr)
+        //console.debug(devConfig)
+    }
+    return devConfig
+}
+
+const runRpaDemo = () => {
+    const {flow_start} = require('../../proj-script/demo-baidu/baidu-visit')
+    let item = {}
+    let browser = {"browserKey":"demo01"}
+    item['browser'] = browser
+    flow_start({item})
 }
 
 const startRpa = async () => {
     console.debug('start rpa ...')
-    console.debug(rpaConfig)
+    //console.debug(rpaConfig)
 
     // 1. load rpa config
     let devConfig = await loadRpaConfig()
-    if(devConfig){
+    if(!!devConfig){
         Object.assign(rpaConfig, devConfig)
     }
+    //console.debug(rpaConfig)
 
     // 2. init
     // 2.1 dataUtil
@@ -39,6 +47,8 @@ const startRpa = async () => {
     browserInit(rpaConfig)
 
     // 3. rpa dev?
+    runRpaDemo()
+
 }
 
 startRpa()
